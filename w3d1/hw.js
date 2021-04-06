d2b43e0144cdcf7e7192a3144974f6a3490b363b
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-04-05 21:44:06 -0400

    Update hw.js

--- w3d1/hw.js
+++ w3d1/hw.js
@@ -1,14 +1,82 @@
 // "use strict";
-let group = {
-  getTitle: function () {
-    return this.title;
+// -----1-----
+function askPassword(ok, fail) {
+  let password = prompt("Password?", "");
+  if (password == "rockstar") ok();
+  else fail();
+}
+let user = {
+  name: "John",
+  loginOk() {
+    alert(`${this.name} logged in`);
   },
+  loginFail() {
+    alert(`${this.name} failed to log in`);
+  },
+};
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+// ----
+// apply works the same way in this case because there is no argument - just replace "call"
+
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?", '');
+//   if (password == "rockstar") ok();
+//   else fail();
+//   }
+//   let user = {
+//   name: 'John',
+//   loginOk() {
+//   alert(`${this.name} logged in`);
+//   },
+//   loginFail() {
+//   alert(`${this.name} failed to log in`);
+//   },
+//   };
+//   askPassword(()=>user.loginOk.call(user), ()=>user.loginFail.call(user));
+// ----2-----
+function askPassword(ok, fail) {
+  let password = prompt("Password?", "");
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: "John",
+
+  login(result) {
+    alert(this.name + (result ? " logged in" : " failed to log in"));
+  },
+};
+
+askPassword(user.login.bind(user, true), user.login.bind(user, false));
+
+// apply works the same way in this case because there is no argument - just replace "call"
+
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?", "");
+//   if (password == "rockstar") ok();
+//   else fail();
+// }
+
+// let user = {
+//   name: "John",
+
+//   login(result) {
+//     alert(this.name + (result ? " logged in" : " failed to log in"));
+//   },
+// };
+
+// askPassword(()=>user.login.call(user, true),()=> user.login.call(user, false));
+// -----3-----
+
+let group = {
   title: "Our Group",
   students: ["John", "Pete", "Alice"],
   showList: function () {
-    this.students.forEach(function (student) {
+    let display = function (student) {
       console.log(this.title + ": " + student);
-    });
+    };
+    this.students.forEach(display.bind(group));
   },
 };
 group.showList();
